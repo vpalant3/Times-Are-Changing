5496547f542ce37dc4da3b7ee983dcb2dfa97525
Vlad <vladislavpalant@gmail.com>
2025-03-16 14:47:21 -0500

    Initial Commit

---
+++ Times Are Changing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Times Are Changing
+//
+//  Created by Vladislav Palant on 3/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
